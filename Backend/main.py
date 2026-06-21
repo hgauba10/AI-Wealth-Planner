b4415a688bd34e04e8eca17555d9cc31d80ca539
Harshit Gauba <harshitgauba2005@gmail.com>
2026-06-21 20:27:56 +0530

    Fix CORS for Vercel frontend

--- Backend/main.py
+++ Backend/main.py
@@ -23,7 +23,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+         "https://ai-wealth-planner.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
